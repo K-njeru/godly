4a9aae2434205b4a9c06cd9bb4c372ec29637100
K-njeru <knjeru519@gmail.com>
2025-02-13 00:47:41 +0300

    `Added CoursesSection component to Page component`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import HeroSection from "@/components/HeroSection";
 import { Footer } from '@/components/footer';
 import AboutSection from '@/components/AboutSection';
 import PartnersSection from '@/components/PartnersSection';
+import CoursesSection from '@/components/CoursesSection';
 
 
 export default function Page() {
@@ -12,6 +13,7 @@ export default function Page() {
       <Navbar />
       <HeroSection />
       <AboutSection />
+      <CoursesSection />
       <PartnersSection />
       <Footer />
       
